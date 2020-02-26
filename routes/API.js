ebb83ac763e9563aff9d52814d70dbe071f4dd08
Petchesi-Iulian <iamnotacat12@gmail.com>
2020-02-26 14:39:51 +0200

    Refs #114576 Added basic removal api.

--- routes/API.js
+++ routes/API.js
@@ -3,6 +3,8 @@ const fs = require('fs-extra')
 const getenv = require('getenv');
 const nconf = require('nconf')
 const screenshoteer = require('screenshoteer')
+var glob = require("glob")
+var util = require('util')
 var good_arguments = ['url', 'w', 'h', 'emulate', 'fullpage', 'pdf', 'waitfor', 'waitforselector', 'auth', 'no', 'click', 'file']
 var location = getenv.string('VOLUME')
 
@@ -24,9 +26,37 @@ exports.create_image = async function(req, res){
     return;
 }
 
-exports.invalidate_cache_for_url = function(req, res){
-    console.log("Test INVALIDATE CACHE FOR URL API");
-    debugger;
+exports.invalidate_cache_for_url = async function(req, res){
+    if (req.query.url === undefined) {
+        res.status(404).send('No url specified');
+        return;
+    }
+    var options = {cwd: location}
+    var count = 0;
+    var url = req.query.url.split('://')[1];
+    try {
+        glob(url + "*.jpg", options, function (er, files) {
+            if (files.length === 0) {
+                res.status(400).send("The url didn't match any file.");
+            }
+            for (i=0; i<files.length; i++) {
+                fs.remove(location + files[i])
+                .then(() => {
+                    console.log('Removed file.');
+                })
+                .catch(err => {
+                    res.status(400).send(err.toString());
+                    console.error(err);
+                })
+                count++;
+            }
+            res.status(200).send(util.format("Finished removing %d files.", count));
+        })
+    }
+    catch(err) {
+        console.log(err);
+        res.status(400).send(err.toString());
+    }
 }
 
 exports.retrieve_image_for_url = async function(req, res){
